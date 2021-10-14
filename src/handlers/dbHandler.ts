d57ba4412f7f028c4284653c03f94a6066b52a08
Pedrohme <pedrohmen45@gmail.com>
2021-10-14 13:59:18 -0300

    feat: switched to using Pool instead of Client for queries (better error handling)

--- src/handlers/dbHandler.ts
+++ src/handlers/dbHandler.ts
@@ -6,65 +6,53 @@ const insertFight = "INSERT INTO fights(fightera, fighterb, winner) VALUES($1, $
 const updateFighter = "UPDATE fighter SET wins = wins+$1, losses = losses+$2 WHERE name = $3";
 
 export class dbHandler {
-    dbClient: pg.Client;
+    dbPool: pg.Pool;
 
     constructor(connectionInfo:any) {
-        this.dbClient = new pg.Client(connectionInfo);
-    }
-    
-    async connect() {
-        try {
-            await this.dbClient.connect();
-            console.log("database connected!");
-
-            this.dbClient.on('error', err => {
-                console.error('database error!', err.stack);
-            });
-
-            return true;
-        } catch (error) {
-            console.log((<Error>error).stack);
-            return false;
-        }
+        this.dbPool = new pg.Pool(connectionInfo);
     }
     
     async insertFighter(values:(string|number)[]) {
         try {
-            const res = await this.dbClient.query(insertFighter, values);
+            const res = await this.dbPool.query(insertFighter, values);
             return res;
         }
         catch (err) {
             console.log((<Error>err).stack);
+            return null;
         }
     }
 
     async selectFighter(values:string[]) {
         try {
-            const res = await this.dbClient.query(selectFighter, values);
+            const res = await this.dbPool.query(selectFighter, values);
             return res;
         }
         catch (err) {
             console.log((<Error>err).stack);
+            return null;
         }
     }
     
     async insertFight(values:string[]) {
         try {
-            const res = await this.dbClient.query(insertFight, values);
+            const res = await this.dbPool.query(insertFight, values);
             return res;
         }
         catch (err) {
             console.log((<Error>err).stack);
+            return null;
         }
     }
     
     async updateFighter(values:(number|string)[]) {
         try {
-            const res = await this.dbClient.query(updateFighter, values);
+            const res = await this.dbPool.query(updateFighter, values);
             return res;
         }
         catch (err) {
             console.log((<Error>err).stack);
+            return null;
         }
     }
 }
