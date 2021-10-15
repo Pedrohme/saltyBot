4acae80768c5ee98fa83e1c334be3ebf63f55d42
Pedrohme <pedrohmen45@gmail.com>
2021-10-15 01:38:37 -0300

    feat: use web api

--- src/handlers/dbHandler.ts
+++ src/handlers/dbHandler.ts
@@ -1,58 +1,68 @@
-import pg from 'pg';
+import got from "got";
 
-const selectFighter = "SELECT * FROM fighter WHERE name = $1";
-const insertFighter = "INSERT INTO fighter(name, wins, losses) VALUES($1, $2, $3)";
-const insertFight = "INSERT INTO fights(fightera, fighterb, winner) VALUES($1, $2, $3)";
-const updateFighter = "UPDATE fighter SET wins = wins+$1, losses = losses+$2 WHERE name = $3";
+export async function insertFighter(values:(string|number)[]) {
+    try {
+        const res = await got.post("http://localhost:3000/api/fighter",  {
+            json: {
+                name: values[0],
+                wins: 0,
+                losses: 0
+            },
+            responseType: 'json'
+        });
+        console.log("status code: ", res.statusCode);
+        return res;
+    } catch (err) {
+        console.log((<Error>err).stack);
+        return null;
+    }
+}
 
-export class dbHandler {
-    dbPool: pg.Pool;
+export async function selectFighter(values:string[]) {
+    try {
+        const res = await got.get(`http://localhost:3000/api/fighter?name=${values[0]}`, {
+            responseType: 'json'
+        });
+        console.log("status code: ", res.statusCode);
+        return res;
+    } catch (err) {
+        console.log((<Error>err).stack);
+        return null;
+    }
+}
 
-    constructor(connectionInfo:any) {
-        this.dbPool = new pg.Pool(connectionInfo);
+export async function insertFight(values:string[]) {
+    try {
+        const res = await got.post("http://localhost:3000/api/fights",  {
+            json: {
+                fightera: values[0],
+                fighterb: values[1],
+                winner: values[2]
+            },
+            responseType: 'json'
+        });
+        console.log("status code: ", res.statusCode);
+        return res;
+    } catch (err) {
+        console.log((<Error>err).stack);
+        return null;
     }
-    
-    async insertFighter(values:(string|number)[]) {
-        try {
-            const res = await this.dbPool.query(insertFighter, values);
-            return res;
-        }
-        catch (err) {
-            console.log((<Error>err).stack);
-            return null;
-        }
-    }
+}
 
-    async selectFighter(values:string[]) {
-        try {
-            const res = await this.dbPool.query(selectFighter, values);
-            return res;
-        }
-        catch (err) {
-            console.log((<Error>err).stack);
-            return null;
-        }
-    }
-    
-    async insertFight(values:string[]) {
-        try {
-            const res = await this.dbPool.query(insertFight, values);
-            return res;
-        }
-        catch (err) {
-            console.log((<Error>err).stack);
-            return null;
-        }
-    }
-    
-    async updateFighter(values:(number|string)[]) {
-        try {
-            const res = await this.dbPool.query(updateFighter, values);
-            return res;
-        }
-        catch (err) {
-            console.log((<Error>err).stack);
-            return null;
-        }
+export async function updateFighter(values:(number|string)[]) {
+    try {
+        const res = await got.put("http://localhost:3000/api/fighter",  {
+            json: {
+                wins: values[0],
+                losses: values[1],
+                name: values[2]
+            },
+            responseType: 'json'
+        });
+        console.log("status code: ", res.statusCode);
+        return res;
+    } catch (err) {
+        console.log((<Error>err).stack);
+        return null;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import dbConnect from './config/config.json';
 import { twitchChatHandler } from './handlers/twitchChatHandler';
-import { dbHandler } from './handlers/dbHandler';
 
 const tmiOptions = {
     connection: {
@@ -10,14 +8,13 @@ const tmiOptions = {
     channels: ['saltybet']
 };
 
-const db = new dbHandler(dbConnect.postgres);
 const twitch = new twitchChatHandler(tmiOptions);
 
 async function main() {
     const twitchConnection = await twitch.connect();
     if (twitchConnection) {
         console.log("ready to start listening!");
-        twitch.startMessageWatcher(db);
+        twitch.startMessageWatcher();
     }
 }
 
