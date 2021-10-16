6753af82402664c06a7df9f57c0796ef4d836494
Pedrohme <pedrohmen45@gmail.com>
2021-10-16 03:38:58 -0300

    added authentication

--- src/handlers/dbHandler.ts
+++ src/handlers/dbHandler.ts
@@ -1,16 +1,52 @@
 import got from "got";
+import {apiBaseUrl, userInfo} from "../config/config.json";
+
+let token:any;
+
+async function authenticate() {
+    try {
+        const res = await got.post(`${apiBaseUrl}login`,  {
+            json: {
+                user: userInfo.name,
+                password: userInfo.password,
+            },
+            responseType: 'json',
+        });
+        console.log("Authentication status code: ", res.statusCode);
+        console.log(res.body);
+        try {
+            if ((<any>res.body).auth) {
+                token = (<any>res.body).token;
+            }
+        }
+        catch (err) {
+            console.log((<Error>err).stack);    
+            token = "";
+        }
+    } catch (err) {
+        console.log((<Error>err).stack);
+    }
+}
+
+authenticate();
 
 export async function insertFighter(values:(string|number)[]) {
     try {
-        const res = await got.post("http://localhost:3000/api/fighter",  {
+        const res = await got.post(`${apiBaseUrl}fighter`,  {
             json: {
                 name: values[0],
                 wins: 0,
                 losses: 0
             },
-            responseType: 'json'
+            responseType: 'json',
+            headers: {
+                "x-access-token" : token
+            }
         });
-        console.log("inser fighter status code: ", res.statusCode);
+        console.log("insert fighter status code: ", res.statusCode);
+        if (res.statusCode === 401 || res.statusCode === 500) {
+            await authenticate();
+        }
         return res;
     } catch (err) {
         console.log((<Error>err).stack);
@@ -20,7 +56,7 @@ export async function insertFighter(values:(string|number)[]) {
 
 export async function selectFighter(values:string[]) {
     try {
-        const res = await got.get(`http://localhost:3000/api/fighter/${values[0]}`, {
+        const res = await got.get(`${apiBaseUrl}fighter/${values[0]}`, {
             responseType: 'json'
         });
         console.log("select fighter status code: ", res.statusCode);
@@ -34,15 +70,21 @@ export async function selectFighter(values:string[]) {
 
 export async function insertFight(values:string[]) {
     try {
-        const res = await got.post("http://localhost:3000/api/fights",  {
+        const res = await got.post(`${apiBaseUrl}fights`,  {
             json: {
                 fightera: values[0],
                 fighterb: values[1],
                 winner: values[2]
             },
-            responseType: 'json'
+            responseType: 'json',
+            headers: {
+                "x-access-token" : token
+            }
         });
         console.log("insert fight status code: ", res.statusCode);
+        if (res.statusCode === 401 || res.statusCode === 500) {
+            await authenticate();
+        }
         return res;
     } catch (err) {
         console.log((<Error>err).stack);
@@ -52,15 +94,21 @@ export async function insertFight(values:string[]) {
 
 export async function updateFighter(values:(number|string)[]) {
     try {
-        const res = await got.put("http://localhost:3000/api/fighter",  {
+        const res = await got.put(`${apiBaseUrl}fighter`,  {
             json: {
                 wins: values[0],
                 losses: values[1],
                 name: values[2]
             },
-            responseType: 'json'
+            responseType: 'json',
+            headers: {
+                "x-access-token" : token
+            }
         });
         console.log("update fighter status code: ", res.statusCode);
+        if (res.statusCode === 401 || res.statusCode === 500) {
+            await authenticate();
+        }
         return res;
     } catch (err) {
         console.log((<Error>err).stack);
